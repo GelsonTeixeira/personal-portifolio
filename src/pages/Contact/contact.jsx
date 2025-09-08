3a80c754d615836107e610a5559622d169109853
Gelson Andrade <student.gelson@gmail.com>
2025-09-08 02:17:32 -0300

    atualizando as logos

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -1,6 +1,11 @@
 import './contact.css'
 import ImageProfile from '../../assets/foto-corporativa.png'
-import Curriculo from '../../assets/curriculo-gelson-andrade.png'
+import Curriculo from '../../assets/curriculo-gelson-andrade.svg'
+
+import GitHubLogo from '../../assets/github-logo.svg'
+import LinkedInLogo from '../../assets/linkedin-logo.svg'
+import InstagramLogo from '../../assets/instagram-logo.svg'
+
 function Contact() {
   return (
     <div id="container">
@@ -15,10 +20,23 @@ function Contact() {
           <p>"Learn more about me by clicking the links"</p>
         </div>
         <div className="container_content_links">
-          <a href="https://github.com/GelsonTeixeira">Git Hub</a>
-          <a href={Curriculo}>Resume us</a>
-          <a href="https://br.linkedin.com/in/gelson-teixeira-andrade">LinkedIn</a>
-          <a href="https://www.instagram.com/gelson_andrade_/?next=%2Ffilipevalerim%2F">Instagram</a>
+          <a href="https://github.com/GelsonTeixeira">
+            <img src={GitHubLogo} alt="GitHub" className="logo github" />
+            GitHub
+          </a>
+          <a href={Curriculo}>
+            <img src={Curriculo} alt="Resume" className="logo resume" />
+            Resume
+          </a>
+
+          <a href="https://br.linkedin.com/in/gelson-teixeira-andrade">
+            <img src={LinkedInLogo} alt="LinkedIn" className="logo linkedin" />
+            LinkedIn
+          </a>
+          <a href="https://www.instagram.com/gelson_andrade_/?next=%2Ffilipevalerim%2F">
+            <img src={InstagramLogo} alt="Instagram" className="logo instagram" />
+            Instagram
+          </a>
         </div>
       </div>
     </div>
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,19 +1,20 @@
 import "./home.css";
 import FotoPerfil from '../../assets/image-perfil.png'
+import Curriculo from '../../assets/curriculo-gelson-andrade.svg'
 import { useNavigate } from "react-router-dom";
-import JavaScriptLogo from '../../assets/javascript-light.png';
-import NodeJsLogo  from '../../assets/nodejs-light.png'
-import HtmlLogo from '../../assets/html-light.png';
-import LogoCss from '../../assets/css-light.png';
-import ReactLogo from '../../assets/reactjs-light.png';
-
+import NodeJsLogo  from '../../assets/node-light.svg'
+import HtmlLogo from '../../assets/html-logo.svg';
+import LogoCss from '../../assets/css-logo.svg';
+import ReactLogo from '../../assets/react-light.svg';
+import PythonLogo from '../../assets/python-light.svg'
+import MongoLogo from '../../assets/mongo-logo.svg'
 
 function Home() {
     const navigate = useNavigate()
     const handleDownload = () => {
       const link = document.createElement('a');
-      link.href = '/assets/curriculo-gelson-andrade.png'; 
-      link.download = 'curriculo-gelson-andrade.png';  
+      link.href = Curriculo; 
+      link.download = 'curriculo-gelson-andrade.svg';  
       link.click();
     };
   return (
@@ -28,18 +29,17 @@ function Home() {
        
           <p>I'm Gelson, a programmer passionate about technology and development. Currently, I am improving my programming skills, creating projects and exploring new solutions to digital challenges. I have experience in various technologies and always seek to evolve, learning and applying new ideas to build functional and efficient interfaces.</p>
           <div className="container_buttons">
-            <button onClick={()=> navigate("/contato")}>Get In Touch</button>
+            <button onClick={()=> navigate("/contact")}>Get In Touch</button>
             <button onClick={handleDownload}>Download CR</button>
           </div>
           <h3>EXPERIENCE WITH</h3>
           <div className="container_img_tecks">
-            <img src={JavaScriptLogo} alt="Logo Javacript" />
+            <img className="react" src={ReactLogo} alt="Logo Reactjs" />
+            <img src={PythonLogo} alt="Logo Python" />
             <img src={NodeJsLogo} alt="Logo Nodejs" />
+            <img src={MongoLogo} alt="Logo Mongo" />
             <img src={HtmlLogo} alt="Logo HTML" />
             <img src={LogoCss} alt="Logo CSS" />
-            <img className="react" src={ReactLogo} alt="Logo Reactjs" 
-          
-              />
           </div>
         </div>
       
